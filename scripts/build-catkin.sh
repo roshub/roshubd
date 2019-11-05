e7fa1ca2080c6f83d57857e2c874c9b9d0487459
Nick Zatkovich <NZatkovich@gmail.com>
2019-11-04 16:56:13 -0800

    Fix debian build

--- scripts/build-catkin.sh
+++ scripts/build-catkin.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+rm -rf debian node_modules/
 bloom-generate rosdebian
 sed -r -e 's/(\t\dh_shlibdeps.*)/\1 -Xnode/' debian/rules
 echo "override_dh_strip:" >>debian/rules
-echo "  dh_strip -Xnode" >>debian/rules
+echo -e "\tdh_strip -Xnode" >>debian/rules
 fakeroot debian/rules binary
